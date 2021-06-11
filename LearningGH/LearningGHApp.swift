bd87068e71de72dd521c3f2fbfffb2237e5fa09b
haotianhuang@gmail.com <HHT964250613nce8>
2021-06-11 16:27:30 +1000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Haotian Huang on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
